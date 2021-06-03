6ce91a438273aeedd978d9b8a6de9e19166f72f0
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-06-03 12:46:00 +0530

    Add files via upload

---
+++ Dynamics Main/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Dynamics_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            dynamic d1, d2, d3, d4, d5;
+            d1 = 10;
+            d2 = 20;
+           // d3 = d1 + d2;
+            d4 = "30";
+            d5 = d1 + d4;
+            Console.WriteLine(d5);
+        }
+    }
+}
