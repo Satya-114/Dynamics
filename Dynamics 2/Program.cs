0cde5b342fbf15383a3a170c089c30f030292e4a
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-06-03 12:42:38 +0530

    Add files via upload

---
+++ Dynamics 2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Dynamics_2
+{
+    class Program
+    {
+        
+        
+            public static void addstr(dynamic s1, dynamic s2)
+            {
+                Console.WriteLine(s1 + s2);
+            }
+
+            // Main method
+            static public void Main()
+            {
+
+                // Calling addstr method
+                addstr("J", "V");
+                addstr("Hi", "Hello");
+                addstr("satya", 1232);
+                addstr(12, 30);
+            }
+            
+    }
+}
